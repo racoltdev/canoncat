f34336c6359403fe1b171ef3e653b06b5a136a91
racoltdev <racoltdev@gmail.com>
2026-09-23 19:31:02 -0400

    shellcheck

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-sudo ln -sf $SCRIPTPATH/canoncat.sh /usr/bin/canoncat
-sudo ln -sf $SCRIPTPATH/canoncat_completion.sh /etc/bash_completion.d/canoncat_completion
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
+sudo ln -sf "$SCRIPTPATH"/canoncat.sh /usr/bin/canoncat
+sudo ln -sf "$SCRIPTPATH"/canoncat_completion.sh /etc/bash_completion.d/canoncat_completion
