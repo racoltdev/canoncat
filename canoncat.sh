3ae8a8da87855635badca5afa03b30e7dd62fb6d
racoltdev <racoltdev@gmail.com>
2026-09-23 22:06:33 -0400

    comments

--- canoncat.sh
+++ canoncat.sh
@@ -6,12 +6,14 @@ if [[ $# -eq 0 ]]; then
 fi
 
 for arg in "$@"; do
+	# $? is exit status from previous command
 	path="$(command -v "${arg}")"; stat=$?
 	if [[ ${stat} -ne 0 ]]; then
-		echo Invalid path argument \'"${arg}"\' ; exit ${stat};
+		echo Invalid path argument \'"${arg}"\' ; exit "${stat}";
 	fi
 
 	realname="$(readlink -f "${path}")"
 	echo "${realname}"
+	# line numbers, close if one screen, don't wipe screen on quit, respond to interrupt
 	less -NFXK "${realname}" || exit $?
 done;
