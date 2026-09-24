4207f361044822716ef4852302944470fc7da5d4
racoltdev <racoltdev@gmail.com>
2026-09-23 23:53:19 -0400

    open all files at once with less for buffer swapping

--- canoncat.sh
+++ canoncat.sh
@@ -5,6 +5,8 @@ if [[ $# -eq 0 ]]; then
 	exit
 fi
 
+declare -a realnames
+index=0
 for arg in "$@"; do
 	# $? is exit status from previous command
 	path="$(command -v "${arg}")"; stat=$?
@@ -12,15 +14,22 @@ for arg in "$@"; do
 		echo Invalid path argument \'"${arg}"\' ; exit "${stat}";
 	fi
 
-	realname="$(readlink -f "${path}")"
-	echo "${realname}"
-	# line numbers, close if one screen, don't wipe screen on quit, respond to interrupt, use verbose prompt
-	less -NFXKM \
-		--line-num-width=4 \
-		--use-color \
-		-DNbk `# line number colors` \
-		-DPBkd `# prompt colors` \
-		-PM"%F | file %i/%m | line %lb/%L \: %Pb\%"$ `# prompt` \
-		"${realname}" || exit $?
+	realnames+=("$(readlink -f "${path}")")
+	index=${index}+1
 
 done;
+
+lessprompt=$'%F | file %i/%m | line %lb/%L \: %Pb\% | \'\:n/p\'=next/prev file, \'q\'=quit'
+# line numbers, close if one screen, don't wipe screen on quit, respond to interrupt, use verbose prompt
+less -NFXM \
+	--line-num-width=4 \
+	--tabs=4 \
+	--use-color \
+	-DNbk `# line number colors` \
+	-DPBkd `# prompt colors` \
+	-PM"${lessprompt}" `# prompt` \
+	"${realnames[@]}"
+
+for name in "${realnames[@]}"; do
+	echo $name
+done
